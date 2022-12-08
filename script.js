dce5a936b38b4f6402241ddab3891b245a4405fb
sadeq-obeydi <sadeqby7@gmail.com>
2022-12-08 16:55:55 +0330

    js crate

---
+++ script.js
@@ -0,0 +1,37 @@
+let icons = document.querySelectorAll("i")
+let txt = document.querySelectorAll('p')
+
+const ul = document.getElementById('ul')
+const circle = document.getElementById('circle')
+
+
+
+ul.addEventListener('click' , function(elemnt){
+    if(elemnt.target.tagName == "I"){
+        activis(elemnt.target)
+        circleTransform(elemnt.target)
+    }
+
+})
+
+function activis (element){
+    icons.forEach((item)=>{
+        if(element === item){
+            item.classList.add("active")
+            item.nextElementSibling.classList.add("active")
+        }
+        else{
+            item.classList.remove("active")
+            item.nextElementSibling.classList.remove("active")
+        }
+    })
+}
+
+function circleTransform(item){
+    let ur = ul.getBoundingClientRect().left;
+    let ir = item.getBoundingClientRect().left;
+
+    console.log(ur);
+    console.log(ir);
+}
+
